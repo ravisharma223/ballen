34a85e620284f565ca0d69e1aefa6c09157fcf1e
Ravi shankar sharma <ravishankarsharma@Ravis-MacBook-Air.local>
2026-02-05 16:00:59 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function moveRandomEl(elm){
-    elm.style.position = "absolute";
-    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
-}
-
-const moveRandom = document.querySelector("#move-random");
-
-moveRandom.addEventListener("mouseenter",function(e){
+function moveRandomEl(elm) {
+    elm.style.position = "fixed"; // screen ke reference se
+  
+    const maxX = window.innerWidth - elm.offsetWidth;
+    const maxY = window.innerHeight - elm.offsetHeight;
+  
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+  
+    elm.style.left = randomX + "px";
+    elm.style.top = randomY + "px";
+  }
+  
+  const moveRandom = document.querySelector("#move-random");
+  
+  moveRandom.addEventListener("mouseenter", (e) => {
     moveRandomEl(e.target);
-})
+  });
